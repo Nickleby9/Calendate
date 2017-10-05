6533654ee8f5fe781d882cce6160dbf8b9b6f6a5
hilayc9 <hilayc9@gmail.com>
2017-10-05 21:41:37 +0300

    fix multidex

--- app/src/main/java/com/calendate/calendate/utils/AppManager.java
+++ app/src/main/java/com/calendate/calendate/utils/AppManager.java
@@ -1,11 +1,11 @@
 package com.calendate.calendate.utils;
 
-import android.app.Application;
+import android.support.multidex.MultiDexApplication;
 
 import com.beardedhen.androidbootstrap.TypefaceProvider;
 import com.google.firebase.database.FirebaseDatabase;
 
-public class AppManager extends Application {
+public class AppManager extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
