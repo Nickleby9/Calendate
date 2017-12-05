7665c09e4c7ae1b6495da9b139e91fd0c45c0e7c
Hilay <hilayc9@gmail.com>
2017-12-05 23:42:36 +0200

    added notifications short splash time add version label

--- app/src/main/java/com/calendate/calendate/AboutDialog.java
+++ app/src/main/java/com/calendate/calendate/AboutDialog.java
@@ -1,15 +1,18 @@
 package com.calendate.calendate;
 
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.calendate.calendate.R;
+import android.widget.TextView;
 
 public class AboutDialog extends DialogFragment {
 
+    TextView tvVersion;
 
     public AboutDialog() {
         // Required empty public constructor
@@ -22,4 +25,18 @@ public class AboutDialog extends DialogFragment {
         return inflater.inflate(R.layout.fragment_about, container, false);
     }
 
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        tvVersion = (TextView) view.findViewById(R.id.tvVersion);
+
+        try {
+            PackageInfo pInfo = getContext().getPackageManager().getPackageInfo(getContext().getPackageName(), 0);
+            String version = pInfo.versionName;
+            tvVersion.setText("v" + version);
+        } catch (PackageManager.NameNotFoundException e) {
+            e.printStackTrace();
+        }
+
+    }
 }
